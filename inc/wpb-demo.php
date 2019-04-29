1d47184037017c5387d05671d4eb181445a8d8b2
Simone Alati <git@simonealati.it>
2019-04-29 14:35:01 +0200

    no message

---
+++ inc/wpb-demo.php
@@ -0,0 +1,31 @@
+<?php
+
+class wpbpdemo extends wpbp {
+
+    const SLUG = 'wpbpdemo';
+
+    public function __construct() {
+        parent::__construct();
+        add_shortcode('wpdb_render', array(&$this, 'render'));
+    }
+
+    /**
+     * Shortcode per il rendering
+     */
+    public static function render($atts, $content = null) {
+        extract(shortcode_atts(array(
+            'par1' => 'Hello',
+            'par2' => 'world'
+            ), $atts,  'render'));
+        ob_start();
+        ?>
+        <p class="helloworld">
+            <?php echo $par1 ?> <?php echo $par2 ?>!
+        </p>
+        <?php
+        return ob_get_clean();
+    }
+
+}
+
+
